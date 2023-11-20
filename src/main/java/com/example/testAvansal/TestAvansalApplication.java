eaa52e1c84398e6c53f7f9c0d8be4b3953c6ee54
Rachid AGBERE <agbererachid@gmail.com>
2023-11-20 08:50:21 +0000

    Starting Point

---
+++ src/main/java/com/example/testAvansal/TestAvansalApplication.java
@@ -0,0 +1,13 @@
+package com.example.testAvansal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAvansalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAvansalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testAvansal/TestAvansalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testAvansal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAvansalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
